be31aa09271ee614c762e112523a7b4628ec83b8
Daniel Salahi <dan.salahii@gmail.com>
2025-03-12 09:05:18 +0300

    Set up initial React application structure with Vite and styling

---
+++ frontend/src/main.tsx
@@ -0,0 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import "./index.css";
+import AppRoutes from "./routes/index.tsx";
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </StrictMode>
+);
